750cb37af38789a220732e6a5c3551b36338c0c5
emilvn <emilvnielsen@hotmail.com>
2023-04-01 20:58:53 +0200

    basic styling and grid

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,41 @@
 "use strict";
 window.addEventListener("load", main);
 
-async function main(){
-    
+async function main() {
+    const DataURL = "test.json";
+    const characters = await fetchData(DataURL);
+    showAllCharacters(characters);
 }
 
-async function fetchData() {
-    
+async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Response not ok");
+        }
+        const data = await response.json();
+        return data;
+    }
+    catch (err) {
+        throw err;
+    }
+}
+
+function showAllCharacters(arr) {
+    for (const obj of arr) {
+        showCharacter(obj);
+    }
+}
+
+function showCharacter(obj) {
+    const myHTML = /*html*/`
+    <article>
+        <img src="${obj.image}">
+        <h2>${obj.name}</h2>
+        <p>a.k.a "${obj.nickname}"</p>
+        <p>${obj.occupation}</p>
+        <p>${obj.age} years old</p>
+    </article>
+    `;
+    document.querySelector("#characters").insertAdjacentHTML("beforeend", myHTML);
 }
